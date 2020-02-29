ff568e527c94a3c3adc3016f18037b18018c26c6
genxhero <genxhero@users.noreply.github.com>
2020-02-28 23:15:55 -0800

    build core app component

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+import TodosBoard from "./TodosBoard";
+
+export default function App() {
+    return (
+        <div className="App">
+            <h1>Wat?</h1>
+            <TodosBoard />
+        </div>
+    );
+}
