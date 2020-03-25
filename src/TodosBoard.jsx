1325f2c3beea8fe14618ea6edd0e1d3e0a3f6ba1
genxhero <genxhero@users.noreply.github.com>
2020-03-24 21:55:17 -0700

    get new card to work

--- src/TodosBoard.jsx
+++ src/TodosBoard.jsx
@@ -9,25 +9,66 @@ const INITIAL_CARDS = {
 }
 
 const TodosBoard = props => {
+
     const moveCardLeft = e => {
-        let to = parseInt(e.target.getAttribute("name"));
+        let to = parseInt(e.currentTarget.getAttribute("name"));
         debugger;
     };
 
     const moveCardRight = e => {
-        let to = parseInt(e.target.getAttribute("name"));
+        let to = parseInt(e.currentTarget.getAttribute("name"));
         debugger;
     };
 
-    const [cards, shiftCards] = useState(INITIAL_CARDS)
+        const newCard = (e) => {
+          const newTask = window.prompt("Please Describe New Task");
+          const columnId = e.currentTarget.getAttribute("name");
+          const newCards = Object.assign({}, cards)
+          newCards[columnId].push(newTask)
+          changeCards(newCards)
+        };
+
+    const [cards, changeCards] = useState(INITIAL_CARDS)
 
     return (
-        <div className="board">
-            <Column columnId={0} cards={cards[0]} employeeName={"Winnie"} color="purple" moveCardLeft={moveCardLeft} moveCardRight={moveCardRight}/>
-            <Column columnId={1} cards={cards[1]} employeeName={"Bob"} color="teal" moveCardLeft={moveCardLeft} moveCardRight={moveCardRight}/>
-            <Column columnId={2} cards={cards[2]} employeeName={"Thomas"} color="blue" moveCardLeft={moveCardLeft} moveCardRight={moveCardRight}/>
-            <Column columnId={3} cards={cards[3]} employeeName={"George"} color="orange" moveCardLeft={moveCardLeft} moveCardRight={moveCardRight} />
-        </div>
+      <div className="board">
+        <Column
+          columnId={0}
+          cards={cards[0]}
+          employeeName={"Winnie"}
+          color="purple"
+          moveCardLeft={moveCardLeft}
+          moveCardRight={moveCardRight}
+          newCard={newCard}
+        />
+        <Column
+          columnId={1}
+          cards={cards[1]}
+          employeeName={"Bob"}
+          color="teal"
+          moveCardLeft={moveCardLeft}
+          moveCardRight={moveCardRight}
+          newCard={newCard}
+        />
+        <Column
+          columnId={2}
+          cards={cards[2]}
+          employeeName={"Thomas"}
+          color="blue"
+          moveCardLeft={moveCardLeft}
+          moveCardRight={moveCardRight}
+          newCard={newCard}
+        />
+        <Column
+          columnId={3}
+          cards={cards[3]}
+          employeeName={"George"}
+          color="orange"
+          moveCardLeft={moveCardLeft}
+          moveCardRight={moveCardRight}
+          newCard={newCard}
+        />
+      </div>
     );
 };
 
