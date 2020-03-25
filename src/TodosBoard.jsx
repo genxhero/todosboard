1461dbd014ea41e9c8f41fd91b8ed1a647fa3997
genxhero <genxhero@users.noreply.github.com>
2020-03-24 21:15:42 -0700

    add usestate to the todosboard

--- src/TodosBoard.jsx
+++ src/TodosBoard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState}from "react";
 import Column from "./Column";
 
 const TodosBoard = props => {
@@ -9,12 +9,15 @@ const TodosBoard = props => {
     const moveCardRight = e => {
 
     };
+
+    const [cards, shiftCards] = useState({0: ["aaa", "bbb"], 1: ["ccc", "ddd"], 2: [ "eee", "fff"], 3: ["ggg", "hhh"]})
+
     return (
         <div className="board">
-            <Column columnId={0} employeeName={"Winnie"} color="purple" moveCardLeft={moveCardLeft} moveCardRight={moveCardRight}/>
-            <Column columnId={1} employeeName={"Bob"} color="teal" moveCardLeft={moveCardLeft} moveCardRight={moveCardRight}/>
-            <Column columnId={2} employeeName={"Thomas"} color="blue" moveCardLeft={moveCardLeft} moveCardRight={moveCardRight}/>
-            <Column columnId={3} employeeName={"George"} color="orange" moveCardLeft={moveCardLeft} moveCardRight={moveCardRight} />
+            <Column columnId={0} cards={cards[0]} employeeName={"Winnie"} color="purple" moveCardLeft={moveCardLeft} moveCardRight={moveCardRight}/>
+            <Column columnId={1} cards={cards[1]} employeeName={"Bob"} color="teal" moveCardLeft={moveCardLeft} moveCardRight={moveCardRight}/>
+            <Column columnId={2} cards={cards[2]} employeeName={"Thomas"} color="blue" moveCardLeft={moveCardLeft} moveCardRight={moveCardRight}/>
+            <Column columnId={3} cards={cards[3]} employeeName={"George"} color="orange" moveCardLeft={moveCardLeft} moveCardRight={moveCardRight} />
         </div>
     );
 };
