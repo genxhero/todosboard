f067d08161fb62c383ef46c78bbc4cecf6640a97
genxhero <genxhero@users.noreply.github.com>
2020-03-25 23:04:37 -0700

    get cards to work

--- src/Card.jsx
+++ src/Card.jsx
@@ -3,17 +3,27 @@ import React from "react";
 const Card = props => {
     const { task, columnId, moveCardLeft, moveCardRight } = props;
     return (
-        <div className="card">
-            <div id="left-shift-arrow" name={columnId} onClick={moveCardLeft}>
-                {columnId > 0 && <span> {"<"}</span>}
-            </div>
-
-            {task}
-
-            <div id="right-shift-arrow" name={columnId} onClick={moveCardRight}>
-                {columnId < 3 && <span> {">"}</span>}
-            </div>
+      <div className="card">
+        <div
+          id="left-shift-arrow"
+          name={columnId}
+          value={task}
+          onClick={moveCardLeft}
+        >
+          {columnId > 0 && <span> {"<"}</span>}
         </div>
+
+        {task}
+
+        <div
+          id="right-shift-arrow"
+          name={columnId}
+          value={task}
+          onClick={moveCardRight}
+        >
+          {columnId < 3 && <span> {">"}</span>}
+        </div>
+      </div>
     );
 };
 
